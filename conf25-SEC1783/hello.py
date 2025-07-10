748ae5c7f29b484917b86ce12f31ca4b239480ed
bwalden-splunk <150752230+bwalden-splunk@users.noreply.github.com>
2025-07-10 15:54:08 -0700

    Add files via upload

---
+++ conf25-SEC1783/hello.py
@@ -0,0 +1,69 @@
+"""
+Example for custom code course
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+################################################################################
+## Global Custom Code Start
+################################################################################
+import re
+
+
+################################################################################
+## Global Custom Code End
+################################################################################
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'add_comment_1' block
+    add_comment_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def add_comment_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_comment_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.comment(container=container, comment="hello world")
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    output = {
+        "output1": [],
+    }
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.save_playbook_output_data(output=output)
+
+    return
